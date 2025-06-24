f2aa78ac36596513ff21a1e4021a0ad7e275720c
ankitbakshi10 <138018097+ankitbakshi10@users.noreply.github.com>
2025-06-24 08:09:21 +0530

    Delete tests directory

--- tests/unit/studentModel.test.js
+++
@@ -1,9 +0,0 @@
-const Student = require("../../models/Student");
-
-describe("Unit: Student Model", () => {
-  it("should create a Student object with name and email", () => {
-    const student = new Student({ name: "Ankit", email: "ankit@example.com" });
-    expect(student.name).toBe("Ankit");
-    expect(student.email).toBe("ankit@example.com");
-  });
-});
